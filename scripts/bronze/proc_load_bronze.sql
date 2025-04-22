4390175f17972aacf4331a2328111f7b67317cf1
vaibhav-shukla-578 <137605002+vaibhav-shukla-578@users.noreply.github.com>
2025-04-22 17:05:22 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -40,7 +40,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> INSERTING DATA INTO: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Vaibhav\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -56,7 +56,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_prd_info;
 		PRINT '>> INSERTING DATA INTO: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Vaibhav\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR =',',
@@ -71,7 +71,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> INSERTING DATA INTO: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Vaibhav\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR =',',
@@ -93,7 +93,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> INSERTING DATA INTO: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Vaibhav\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		FROM 'C:\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR =',',
@@ -109,7 +109,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> INSERTING DATA INTO: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Vaibhav\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		FROM 'C:\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR =',',
@@ -124,7 +124,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> INSERTING DATA INTO: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Vaibhav\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM 'C:\Data Engineering\Projects\SQL Data Warehouse Project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR =',',
